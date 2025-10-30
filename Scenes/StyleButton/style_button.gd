7280e2ba15054322fd386e400e66c1442ef50397
Terence Clayton-Harvey <166267115+LovelyCloudGames@users.noreply.github.com>
2025-10-30 13:34:57 -0400

    Began creating a custom 'StyleButton' for the editor ui.

---
+++ Scenes/StyleButton/style_button.gd
@@ -0,0 +1,9 @@
+@tool
+extends Button
+
+@export var panel:Panel
+@export var font:FontVariation
+@export var styleBoxFlat:StyleBoxFlat
+@export var highlightColor:Color
+@export var highlightInTime:float = 0.1
+@export var highlightOutTime:float = 0.5
