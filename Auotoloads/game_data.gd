e8c36ee0d545d22440f4ed839d2b22c53199b820
unknown <terenceclayton123423@gmail.com>
2025-11-06 00:20:37 -0500

    Started making a parallaxGUI class for the main menu, but more laptop issues :(

--- Auotoloads/game_data.gd
+++ Auotoloads/game_data.gd
@@ -1,12 +1,16 @@
 extends Node
 
-var userAbsolutePath:String = OS.get_user_data_dir()
-
 const beatsPerRotation:int = 4
 const spawnSide:int = -1
 const radiusInPixels:float = 450.0
 
+var userAbsolutePath:String = OS.get_user_data_dir()
+
 func _ready() -> void:
+	# Load the user config file
 	FileLoader.load_user_config()
 	# Create the "user://maps" folder
 	DirAccess.make_dir_absolute(userAbsolutePath.path_join("maps"))
+
+func _process(_delta: float) -> void:
+	get_window().mode = Window.MODE_FULLSCREEN
