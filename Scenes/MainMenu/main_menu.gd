d13de003e6051bdd5363a3d2d9e7e895c46e4792
unknown <terenceclayton123423@gmail.com>
2025-11-07 20:40:26 -0500

    Imported code for control-based parallax from reddit user u/TheOrioli and used it for ParallaxGUI.

--- Scenes/MainMenu/main_menu.gd
+++ Scenes/MainMenu/main_menu.gd
@@ -1,6 +1,20 @@
 extends Control
 class_name MainMenu
 
-@export var parallax:Parallax2D
-@export var enableParallax:bool = true
-@export var parallaxStrngth:float = 1.0
+@export var parallax:ParallaxGUI
+
+var _enableParallax:bool
+@export var enableParallax:bool = true:
+	set(value):
+		if _enableParallax != value:
+			_enableParallax = value
+			toggle_parallax(value)
+	get:
+		return _enableParallax
+
+func toggle_parallax(value):
+	if parallax:
+		if value == false:
+			parallax.maximumMovement = Vector2.ZERO
+		else:
+			parallax.maximumMovement = Vector2(8.0, 8.0)
--- Scenes/ParallaxGUI/parallax_gui.gd
+++ Scenes/ParallaxGUI/parallax_gui.gd
@@ -1,19 +1,44 @@
 extends Control
 class_name ParallaxGUI
 
+@export var controls: Array[Control]
+@export var maximumMovement: Vector2 = Vector2(8.0, 8.0)
+@export var movementDecrease: float = 0.5
 
-@export var parallaxStrngth:float = 0.5
-@export var smoothness:float = 0.5
-@export_category("Override")
-@export var followMouse:bool = true
+var previousScreenPercent: Vector2 = Vector2(0,0)
 
-var targetOffset:Vector2 = Vector2.ZERO
+func _ready() -> void:
+	get_viewport().gui_focus_changed.connect(_on_focus_changed)
+	
 
 func _process(_delta: float) -> void:
-	move_children()
+	scale_controls()
+	set_parallax(calculate_screen_percent(get_global_mouse_position()))
 
-func move_children():
-	var viewportSize = get_viewport_rect()
-	if followMouse:
-		for child in self.get_children():
-			pass
+func calculate_screen_percent(globalPos: Vector2) -> Vector2 :
+	var viewportSize = get_viewport_rect().size
+	var p = globalPos/ viewportSize - Vector2(0.5,0.5)
+
+	p.x = clamp(p.x, -0.5, 0.5)
+	p.y =  clamp(p.y, -0.5, 0.5)
+	return p
+
+func set_parallax(screenPercent: Vector2) -> void:
+	var move = maximumMovement
+	if controls.size() == 0:
+		assert(controls.size() > 0, "Exported variable 'controls' has no members. ParallaxGUI")
+		return
+	for i in range(controls.size()):
+		var c := controls[i]
+		c.position = move * screenPercent
+		move = move * movementDecrease
+	previousScreenPercent = screenPercent
+
+func _on_focus_changed(node: Control) -> void:	
+	var p =calculate_screen_percent(node.global_position)
+	var t = create_tween()
+	t.tween_method(set_parallax, previousScreenPercent,  p, 0.25 )
+
+func scale_controls():
+	for control:Control in controls:
+		control.pivot_offset = Vector2(control.size/2)
